91fb80b579bcfb35b11b9d0974e2c8d5979d3b49
Mattex125 <matteomerollim1@gmail.com>
2025-03-20 19:30:21 +0100

    creazione main file

--- main.c
+++ main.c
@@ -2,4 +2,10 @@
 //come dif, patch ect...
 //github è un insieme di server che consentono di poter condividere in rete il nostro codice
 
-sono una modifica a caso
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+    
+    return 0;
+}
