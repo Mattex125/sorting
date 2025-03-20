ae388f645c36312ede671e90544a95951fda045b
Mattex125 <matteomerollim1@gmail.com>
2025-03-20 14:42:21 +0100

    sono una modifica nel nuovo brench

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
 //fare versioning anche localmente --> git (un software gestione degli aggiornamenti di un software)
 //come dif, patch ect...
 //github è un insieme di server che consentono di poter condividere in rete il nostro codice
+
+sono una modifica a caso
